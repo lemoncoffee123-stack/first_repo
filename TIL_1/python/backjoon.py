3a7550068226d4768f89edbc217af4d7f18494bd
sela475 <sela475@naver.com>
2026-03-27 13:33:03 +0900

    AI folder upload

--- TIL_1/python/backjoon.py
+++ TIL_1/python/backjoon.py
@@ -1,28 +1,14 @@
-import sys
-input = sys.stdin.readline
+words = ["G...", ".I.T", "..S."]
 
-N = int(input().strip())
-table = list(map(int, input().split()))
-size = int(N ** 0.5)
-cnt = [0] * 1000001
-dist_cnt = [0] * (N + 1)
-bucket = [0] * (N//size + 1)
+K = int(input())
+result = []
 
-for i in table:
-    cnt[i] += 1
+for i in range(3):
+    word = ""
+    for j in range(4):
+        for _ in range(K):
+            word += words[i][j]
+    for _ in range(K):
+        result.append(word)
 
-for val in cnt:
-    if val > 0:
-        dist_cnt[val] += 1
-
-for i in range(1, N + 1):
-    bucket[i // size] += dist_cnt[i]
-
-M = int(input().strip())
-for _ in range(M):
-    a, b = map(int, input().split())
-    count = 0
-
-    left, right = a, b
-    while a <= b and a % size != 0:
-        count +=
+print(*result, sep="\n")
